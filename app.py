cb33346caeaba0adc95aae4d5bebc5a20c883b72
MilianDIngco <mingco13@gmail.com>
2025-05-24 21:20:04 -0400

    Added menu displaying

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import termios
 import tty
 import threading
 import time
+from typing import List
 import pyfiglet
 
 '''
@@ -34,51 +35,177 @@ block = '█'
 size = os.get_terminal_size()
 screen = ""
 
-def cursor_to(row, col):
-    return f"\033[{row};{col}H"
+class Routine:
 
-def draw_block(row, col):
-    str = cursor_to(row, col) + block
-    return str
+    def __init__(self, name):
+        self.name = name
+        self.tasks = []
 
-def draw_char(row, col, char):
-    str = cursor_to(row, col) + char
-    return str
+    def add_task(self, name, duration):
+        self.tasks.append((name, duration))
 
-def draw_rect(row, col, width, height):
-    str = cursor_to(row, col)
-    for _ in range(width):
-        str += block
+    def edit_task(self, orig_name, new_name = "", new_duration = -1):
+        index = -1
+        for i, task in enumerate(self.tasks):
+            if task[0] == orig_name:
+                index = i
+                break
 
-    for i in range(height):
-        current_row = row + 1 + i
-        str += cursor_to(current_row, col)
-        str += block
-        str += cursor_to(current_row, col + width - 1)
-        str += block
+        if (new_name != ""):
+            self.tasks[1] = new_name
+        if (new_duration != -1):
+            self.tasks[0] = new_duration
 
-    str += cursor_to(row + height + 1, col)
-    for _ in range(width):
-        str += block
+    def edit_name(self, new_name):
+        self.name = new_name
 
-    return str
+    #def start_routine(self):
 
-def get_center_row(height):
-    return (int) (size.lines / 2 - height / 2)
+class Screen:
+    v_padding = 2
+    h_padding = 5
+    
+    def __init__(self):
+        self.screen = ""
+        self.menus = []
 
-def get_center_col(width):
-    return (int) (2 + size.columns / 2 - width / 2)
+    @staticmethod
+    def cursor_to(row, col):
+        return f"\033[{row};{col}H"
 
-def set_fg_col(r, g, b):
-    return f"\033[38;2;{r};{g};{b}m"
+    @staticmethod
+    def get_center_row(height):
+        return (int) (size.lines / 2 - height / 2)
 
-def set_bg_col(r, g, b):
-    return f"\033[48;2;{r};{g};{b}m"
+    @staticmethod
+    def get_center_col(width):
+        return (int) (2 + size.columns / 2 - width / 2)
 
-def add_screen(value):
-    global screen
-    screen += value 
 
+    def draw_block(self, row, col):
+        self.screen += self.cursor_to(row, col) + block
+        
+    def draw_char(self, row, col, char):
+        self.screen += self.cursor_to(row, col) + char
+
+    def draw_rect(self, row, col, width, height):
+        str = self.cursor_to(row, col)
+        for _ in range(width):
+            str += block
+
+        for i in range(height):
+            current_row = row + 1 + i
+            str += self.cursor_to(current_row, col)
+            str += block
+            str += self.cursor_to(current_row, col + width - 1)
+            str += block
+
+        str += self.cursor_to(row + height + 1, col)
+        for _ in range(width):
+            str += block
+
+        self.screen += str
+
+    def set_fg_col(self, r, g, b):
+        self.screen += f"\033[38;2;{r};{g};{b}m"
+
+    def set_bg_col(self, r, g, b):
+        self.screen += f"\033[48;2;{r};{g};{b}m"
+
+    def add_screen(self, value):
+        self.screen += value 
+
+    def clear_screen(self):
+        print("\033[2J\033[H", end='')
+
+    def display_menu(self, menu):
+        options = menu.options
+
+        max_width = len(menu.title)
+        for option in options:
+            max_width = len(option[0]) if len(option[0]) > max_width else max_width
+        
+        max_width += self.h_padding * 2
+        max_height = len(menu.options) + 1 + 1 + self.v_padding * 2
+
+        row = self.get_center_row(max_height)
+        col = self.get_center_col(max_width)
+
+        # Print box around menu
+        self.draw_rect(row, col, max_width, max_height)
+
+        # Print title
+        title_row = row + self.v_padding + 1
+        title_col = self.get_center_col(len(menu.title))
+        title = self.cursor_to(title_row, title_col) + menu.title
+        self.add_screen(title)
+        
+        # Print marker then options
+        option_col = col + self.h_padding - 1
+        option_row = title_row + 2
+        self.add_screen(self.cursor_to(option_row, option_col))
+        for i, option in enumerate(options):
+            if i == menu.selected:
+                marker = "# "
+            else:
+                marker = option[1] + " "
+            words = option[0]
+            self.add_screen(marker + words) 
+            
+            # move to next
+            option_row += 1
+            self.add_screen(self.cursor_to(option_row, option_col))
+            
+        """
+        ███████████
+        █         █
+        █  title  █
+        █         █
+        █  1      █
+        █  2      █
+        █  3      █
+        █         █
+        ███████████
+        """
+
+    def update_screen(self):
+        self.clear_screen()
+        print(self.screen, end='')
+        self.screen = ""
+
+class Menu:
+
+    """
+    Options 
+    [ ("name", "marker", lambda) ]
+    """
+    def __init__(self, title):
+        self.title = title
+        self.options = []
+        self.selected = 0
+
+    def add_option(self, options, index = -1):
+        if (index == -1):
+            index = len(self.options)
+
+        if (type(options) is list):
+            for opt in options:
+                self.options.insert(index, opt)
+                index += 1
+        else:
+            self.options.insert(index, options)
+
+    def up(self):
+        self.selected = (self.selected - 1) % len(self.options)
+
+    def down(self):
+        self.selected = (self.selected + 1) % len(self.options)
+
+    def select(self):
+        self.options[self.selected][2]()
+
+
+    
 #def progress_bar(row, col, width, percent):
     
 
@@ -95,18 +222,10 @@ def get_key():
     return ch
 
 # Clear the screen
-def clear_screen():
-    print("\033[2J\033[H", end='')
 
 # Draw menu with selection highlighting
-def draw_menu(selected_index):
-    clear_screen()
-    options = ["Option 1", "Option 2", "Quit"]
-    print("=== Basic Python TUI ===\n")
-    for i, option in enumerate(options):
-        prefix = "=> " if i == selected_index else "   "
-        print(f"{prefix}{option}")
 
+'''
 def main():
     selected = 0
     options = 3
@@ -122,24 +241,33 @@ def main():
             selected = (selected + 1) % options
         elif key in ('\n', '\r'):  # Enter key
             if selected == 0:
+        self.cursor_to(
                 print("\nYou selected Option 1")
             elif selected == 1:
                 print("\nYou selected Option 2")
             print("Press any key to continue... yurt█")
             get_key()
             draw_block(10, 10)
+'''
 
 if __name__ == "__main__":
     #main()
-    clear_screen()
-    width = size.columns - 10
-    height = size.lines - 10
-    screen = ""
+    screen = Screen()
+    screen.clear_screen()
     
-    add_screen(set_fg_col(55, 100, 50))
-    add_screen("hello")
-    add_screen(draw_rect(get_center_row(height), get_center_col(width), width, height))
-    print(screen)
+    main_menu = Menu("Main Menu")
+    main_menu.add_option([
+        ("Morning routine", "*", lambda x: x*x),
+        ("Morning routine", "*", lambda x: x*x),
+        ("Morning routine", "*", lambda x: x*x),
+        ("Morning routine", "*", lambda x: x*x),
+        ("Morning routine", "*", lambda x: x*x),
+        ("Morning routine", "*", lambda x: x*x),
+        ("Add new routine", "*", lambda x: x * x),
+        ("Quit", "~", lambda x: x * x)
+    ])
 
-    ascii_banner = pyfiglet.figlet_format("Title Bann")
-    print(ascii_banner)
+    screen.display_menu(main_menu)
+    screen.update_screen()
+
+
